146f754396fdc513b0a8c8c5b686ca549b08f288
ReeceRenninger <renningerreece@gmail.com>
2024-04-25 18:42:52 -0500

    notes for max sub array, and completed product of array except self

--- Python/MaxSubArray/main.py
+++ Python/MaxSubArray/main.py
@@ -1,10 +1,10 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         
-        runningTotal = nums[0]
-        maxSum = nums[0]
+        runningTotal = nums[0] #initalize value at nums[0] index position
+        maxSum = nums[0] #initalize value at nums[0] index position
 
-        for num in nums[1:]:
-            runningTotal = max(num, runningTotal + num)
-            maxSum = max(maxSum, runningTotal)
+        for num in nums[1:]: # for loop starting at index 1 position since we set runningTotal and maxSum as start of nums
+            runningTotal = max(num, runningTotal + num) #use max to grab either the largest num or total of runningTotal + num
+            maxSum = max(maxSum, runningTotal) # use max to assign maxSum or runningTotal depending on which is larger
         return maxSum
